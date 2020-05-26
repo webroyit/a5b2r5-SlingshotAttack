dba3a7c5c9ed7a468fcd92b44bde9d64f4590d51
royweb <xytank@gmail.com>
2020-05-26 14:30:03 -0400

    Enable isKinematic on the Ball when mouse down to move the ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,16 +2,23 @@
 
 public class Ball : MonoBehaviour
 {
+    // Note: drag and drop Ball's Rigidbody2D component to this variable in Unity
+    public Rigidbody2D rb;
+
     private bool isPressed = false;
 
     void OnMouseDown()
     {
         isPressed = true;
+
+        // To move the ball
+        rb.isKinematic = true;
     }
 
     void OnMouseUp()
     {
         isPressed = false;
+        rb.isKinematic = false;
     }
 
 }

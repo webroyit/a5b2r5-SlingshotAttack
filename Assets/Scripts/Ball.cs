bf170a7cc831e28f3d9fbc7ad9c693479141aac3
royweb <xytank@gmail.com>
2020-05-26 14:19:59 -0400

    Add functions to listen for mouse input

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,5 +2,16 @@
 
 public class Ball : MonoBehaviour
 {
-    
+    private bool isPressed = false;
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+    }
+
 }

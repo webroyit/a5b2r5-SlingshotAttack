81cc404f8d543909032b370d2a553e3f3e4021f0
royweb <xytank@gmail.com>
2020-05-27 14:11:04 -0400

    Use relativeVelocity to get the velocity of the collision for the enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    // This is a callback
+    void OnCollisionEnter2D(Collision2D colInfo)
+    {
+        // relativeVelocity is a 2-Dimensional vector
+        // It compared the velocity of one object to the other object
+        // magnitude convert them into single number
+        Debug.Log(colInfo.relativeVelocity.magnitude);
+    }
 }

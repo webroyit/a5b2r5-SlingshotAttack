766dbff3bc4a33885fba5c703c09aa3c19672d65
royweb <xytank@gmail.com>
2020-05-27 14:22:42 -0400

    Remove the enemy if the impact is greater than enemy's health

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float health = 4f;
+
     // This is a callback
     void OnCollisionEnter2D(Collision2D colInfo)
     {
         // relativeVelocity is a 2-Dimensional vector
         // It compared the velocity of one object to the other object
         // magnitude convert them into single number
-        Debug.Log(colInfo.relativeVelocity.magnitude);
+        if(colInfo.relativeVelocity.magnitude > health)
+        {
+            // Enemy is dead
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }

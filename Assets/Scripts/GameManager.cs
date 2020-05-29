f85df100ab57d2431d32b6eddbf52fe1386888b9
royweb <xytank@gmail.com>
2020-05-29 11:45:42 -0400

    Refactor Enemy class and move some of the logic to GameManager class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,5 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    private int EnemiesAlive = 0;
+
+    public void IncreaseEnemyCount()
+    {
+        EnemiesAlive++;
+    }
+
+    public void DecreaseEnemyCount()
+    {
+        EnemiesAlive--;
+
+        // If there is no more enemies, load the next level
+        if(EnemiesAlive <= 0)
+        {
+            // Load the next level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 }
